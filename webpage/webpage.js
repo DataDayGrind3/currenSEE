96d0cc07e75ff838672e4f73376b570a737f2c3b
e0425705 <puahxiuwen@gmail.com>
2022-07-24 13:15:08 +0800

    add voice over

--- webpage/webpage.js
+++ webpage/webpage.js
@@ -1,7 +1,30 @@
 function start() {
     window.location.href = "../index.html";
+
+    let speech = new SpeechSynthesisUtterance();
+    speech.lang = "en-US";
+    speech.text = "Starting currenSEE";
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    window.speechSynthesis.speak(speech);
 }
 
 function help() {
-    window.location.href = "../index.html";
+    let speech = new SpeechSynthesisUtterance();
+    speech.lang = "en-US";
+    speech.text = "This application aims to help you check your currency. All you have to do is take a picture of your currency, either front or back and we will do the processing, letting you know the currency value.";
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    window.speechSynthesis.speak(speech);
+}
+
+function welcome() {
+    let speech = new SpeechSynthesisUtterance();
+    speech.lang = "en-US";
+    speech.text = "Welcome to currenSEE. Click left to start and click right for more about this application.";
+    speech.rate = 1;
+    speech.pitch = 1;
+    window.speechSynthesis.speak(speech);
 }

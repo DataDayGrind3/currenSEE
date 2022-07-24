94d68a86d4cef67cf2013e8d56967c13ce055eab
e0425705 <puahxiuwen@gmail.com>
2022-07-24 13:28:23 +0800

    remove loop from welcome message

--- webpage/webpage.js
+++ webpage/webpage.js
@@ -13,7 +13,7 @@ function start() {
 function help() {
     let speech = new SpeechSynthesisUtterance();
     speech.lang = "en-US";
-    speech.text = "This application aims to help you check your currency. All you have to do is take a picture of your currency, either front or back and we will do the processing, letting you know the currency value.";
+    speech.text = "This application aims to help you check your currency. All you have to do is take a picture of your currency, either front or back and we will do the processing, letting you know the currency value. Click anywhere on the left side of the current screen to start.";
     speech.volume = 1;
     speech.rate = 1;
     speech.pitch = 1;
